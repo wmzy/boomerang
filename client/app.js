d75d32ee13d62786ee1a7e75e2ecf6aefe9a75a2
wmzy <1256573276@qq.com>
2017-03-28 23:03:28 +0800

    use URLON stringfiy json

--- client/app.js
+++ client/app.js
@@ -1,6 +1,18 @@
-document.onreadystatechange = function () {
-  if(document.readyState == 'complete') {
-    var editor = document.getElementById('editor');
-    console.log(editor);
-  }
-};
+import URLON from 'URLON';
+document.addEventListener('DOMContentLoaded', function () {
+  var editor = document.getElementById('editor');
+  var output = document.getElementById('output');
+
+  editor.addEventListener('change', () => {
+    try {
+      var str = URLON.stringify(JSON.parse(editor.value));
+      var link = 'http://localhost:3000/raw/' + str;
+      console.log(link);
+      output.href = link;
+      output.innerText = link;
+    } catch (e) {
+      output.href = '#';
+      output.innerText = '#'
+    }
+  })
+});
--- server/app.js
+++ server/app.js
@@ -2,14 +2,21 @@ const path = require('path');
 const Koa = require('koa');
 const staticServe = require('koa-static');
 const bodyParser = require('koa-bodyparser');
+const Router = require('koa-router');
+const URLON = require('URLON');
 
+const router = new Router();
+
+router
+  .all('/raw/:jsonString', ctx => {
+    ctx.body = URLON.parse(ctx.params.jsonString);
+  });
 
 const app = new Koa();
 
 app.use(staticServe(path.resolve(__dirname, '../public')));
-app.use(async ctx => {
-  ctx.body = {};
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(3000);
 console.log('Listening on port 3000');
