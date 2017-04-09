9803f3aca52169454eca23fb10a24e56f98e23f4
wmzy <1256573276@qq.com>
2017-04-09 18:28:13 +0800

    schema faker

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const Router = require('koa-router');
 const URLON = require('urlon');
+const jsf = require('json-schema-faker');
 
 const router = new Router();
 
 router
   .all('/raw/:jsonString', ctx => {
     ctx.body = URLON.parse(ctx.params.jsonString);
-  });
+  })
+  .all('/schema/:jsonString', ctx => {
+    const schema = URLON.parse(ctx.params.jsonString);
+    ctx.body = jsf(schema);
+  })
+;
 
 const app = new Koa();
 app.use(router.routes());
